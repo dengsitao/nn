eae0d1bd6ca04552746c8bf5aa9f53669295dbe7
SitaoDeng <sitao.deng@polycom.com>
2017-04-24 19:40:39 +0800

    still sth. wrong

--- mynn.py
+++ mynn.py
@@ -1,6 +1,7 @@
 import struct
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 ## compute sigmoid nonlinearity
@@ -29,130 +30,148 @@ def relu(x):
 
 def relu_deri(output):
     return 1.*(output>0)
-def show(image):
-    """
-    Render a given numpy.uint8 2D array of pixel data.
-    """
-    from matplotlib import pyplot
-    import matplotlib as mpl
-    fig = pyplot.figure()
-    ax = fig.add_subplot(1,1,1)
-    imgplot = ax.imshow(image, mpl.cm.Greys)
-    imgplot.set_interpolation('nearest')
-    ax.xaxis.set_ticks_position('top')
-    ax.yaxis.set_ticks_position('left')
-    pyplot.show()
 
-alpha = 0.01
+def costFunc(h, y):
+    m=h.shape[0]
+    return np.sum(pow((h-y),2))/m
+
+def costFuncReg(self, h, y):
+    cost=self.costFunc(h, y)
+    return cost #+ ((self.lda/2)*(np.sum(pow(self.ih,2)) + np.sum(pow(self.ho,2))))
+
+alpha = 0.03
+lamda = 0.1#alpha*alpha
 input_dim = 28*28
 hidden_dim1 = 500
 hidden_dim2 = 100
 output_dim = 10
 # initialize neural network weights
-synapse_0 = (2*np.random.random((input_dim,hidden_dim1))-1)
-synapse_1 = (2*np.random.random((hidden_dim1,hidden_dim2))-1)
-synapse_2 = (2*np.random.random((hidden_dim2,output_dim)) -1)
-#if os.path.isfile('home/rdeng/code/nn/myparam0'):
-   
-
-synapse_0_update = np.zeros_like(synapse_0)
-synapse_1_update = np.zeros_like(synapse_1)
-synapse_2_update = np.zeros_like(synapse_2)
+weight0 = (2*np.random.random((input_dim+1,hidden_dim1))-1)
+weight1 = (2*np.random.random((hidden_dim1+1,hidden_dim2))-1)
+weight2 = (2*np.random.random((hidden_dim2+1,output_dim)) -1)
 bias_0=np.zeros((1, hidden_dim1))
 bias_1=np.zeros((1, hidden_dim2))
 bias_2=np.zeros((1, output_dim))
+d0=np.zeros(weight0.shape)
+d1=np.zeros(weight1.shape)
+d2=np.zeros(weight2.shape)
+#imagef = open('/home/rdeng/code/mine/nn/data/train-images-idx3-ubyte', 'rb')
+#labelf = open('/home/rdeng/code/mine/nn/data/train-labels-idx1-ubyte', 'rb')
 
 imagef = open('/home/rdeng/code/mine/nn/data/train-images-idx3-ubyte', 'rb')
 labelf = open('/home/rdeng/code/mine/nn/data/train-labels-idx1-ubyte', 'rb')
+magic, imgNum=struct.unpack(">II", imagef.read(8))
+imgRow, imgCol =struct.unpack(">II", imagef.read(8))
+print magic, imgNum, imgRow, imgCol
+lblMagic, lblNum=struct.unpack(">II", labelf.read(8))
+print lblMagic, lblNum
+overallError = 0
+Xa=np.zeros((imgNum, imgRow*imgCol))
+Ya=np.zeros((lblNum, 1))
+print '----start read data----'
+for i in range(imgNum):
+    Xa[i, range(imgRow*imgCol)]=np.fromfile(imagef, np.uint8, imgRow*imgCol)
+    Ya[i, 0]=np.fromfile(labelf, np.uint8, 1)
 
-for k in range(1):
-    if k>1:
-        imagef.close()
-        labelf.close()
-    imagef = open('/home/rdeng/code/mine/nn/data/train-images-idx3-ubyte', 'rb')
-    labelf = open('/home/rdeng/code/mine/nn/data/train-labels-idx1-ubyte', 'rb')
-    magic, imgNum=struct.unpack(">II", imagef.read(8))
-    imgRow, imgCol =struct.unpack(">II", imagef.read(8))
-    print magic, imgNum, imgRow, imgCol
-    lblMagic, lblNum=struct.unpack(">II", labelf.read(8))
-    print lblMagic, lblNum
-    overallError = 0
-    for j in range(imgNum):
-    #for j in range(1):
-        #read a 28x28 image and a byte label
-        X=np.fromfile(imagef, np.uint8, imgRow*imgCol)
-        y=np.fromfile(labelf, np.uint8, 1)
-        # where we'll store our best guess (binary encoded)
-        d = np.zeros_like(y)
-        #img = X.reshape(imgRow, imgCol)
-        #show(img)
-        X=X.reshape(1, imgRow*imgCol)
-        #print 'X= ',X.shape
-        #print 'y=',y
-        #print 'y.shape= ',y.shape, 'y = ', y
-        #print 'synapse_0= ',synapse_0.shape
+print '----finish read data----'
+valiNum=10000
+loop=imgNum-valiNum
+errordot=np.zeros((loop,1))
+
+for k in range(3):
+    for j in range(loop):
+        X=Xa[j]
+        X=X.reshape(1,imgRow*imgCol)
+        X=np.c_[[1],X]
+        y=Ya[j]
+        #print 'y=',y,'y[0]=',y[0]
         #Forward propagation
-        z1=np.dot(X, synapse_0)
-        f1=sigmoid(z1+bias_0)
-        #f1=relu(z1+bias_0)
-        #print 'f1= ',f1.shape
-        #print 'synapse_1= ',synapse_1.shape
-        z2=np.dot(f1, synapse_1)
-        f2=sigmoid(z2+bias_1)
-    
-        z3=np.dot(f2, synapse_2)
-        f3=sigmoid(z3+bias_2)
-        #p3=-np.log(softmax(f3))
+        z1=np.dot(X, weight0)
+        f1=sigmoid(z1)#+bias_0)
+        f1=np.c_[[1], f1]
+        z2=np.dot(f1, weight1)
+        f2=sigmoid(z2)#+bias_1)
+        f2=np.c_[[1],f2] 
+        #print 'weight2=',weight2.shape,'f2=',f2.shape
+        z3=np.dot(f2, weight2)
+        f3=sigmoid(z3)#+bias_2)
+        sumf3=np.sum(f3)
+        #print 'sumf3',sumf3
         p3=softmax(f3)
-        #p3=softmax(f2)
         yy=np.zeros((1, output_dim))
-        yy[0,y]=1
-        if j%6000==0:
-            print 'predict=',p3
+        yy[0,y[0]]=sumf3
     
         #backward propagation
-        error=p3-yy
+        error=pow(f3-yy,2)/2
+        #error=costFunc(p3, yy)
+        #print 'eroor=',error
         overallError+=np.sum(np.abs(error))
-        #print 'y=',y, 'f3=',f3, 'error=',error
+        errordot[j]=np.sum(error)
+    
         gprime=sigm_deri(f3)
-        #gprime=softmax_deri(f3)
-        #print 'gprime=',gprime.shape,'=sigma_deri(f3), f3=',f3.shape
-        #error=data_loss
-        delta3=error*gprime
-        #print 'delta3=',delta3.shape,'sy_2.T=',synapse_2.T.shape
-        delta2=np.dot(delta3, synapse_2.T)*sigm_deri(f2)
-        #delta2=np.dot(synapse_2.T, delta3)*sigm_deri(f2)
-        #print 'delta2=',delta2.shape,'delta3xs2.T,s2.T=',synapse_2.T.shape,'sgder(f2)=',sigm_deri(f2).shape
-        delta1=np.dot(delta2, synapse_1.T)*sigm_deri(f1)
-        #print 'delta1=',delta1.shape,'delta2xs1,s1=',synapse_1.T.shape,'sgder(f1)=',sigm_deri(f1).shape
-    
-        #print 'delta3=',delta3.shape,'f2.T=',f2.T.shape
-        #print 'delta3.T=',delta3.T.shape,'f2=',f2.shape
-        #d2=np.dot(f2.T,delta3)/output_dim
-        d2=np.dot(delta3.T, f2)
+        delta3=error#*gprime
+        #print 'delta3=',delta3.shape,'weight2.T=',weight2.T.shape,'f2=',f2.shape
+        delta2=np.dot(delta3, weight2.T)*sigm_deri(f2)
+        delta2=delta2[0,1:]
+        delta2=delta2.reshape(1, hidden_dim2)
+        #print 'delta2=',delta2.shape,'weight1.T=',weight1.T.shape,'f1=',f2.shape
+        delta1=np.dot(delta2, weight1.T)*sigm_deri(f1)
+        #print 'd2=',d2.shape,'delta3.T=',delta3.T.shape,'f2=',f2.shape
+        delta1=delta1[0,1:]
+        delta1=delta1.reshape(1, hidden_dim1)
+        d2+=np.dot(delta3.T, f2).T
         dbias_2=delta3
-        #print 'delta2=',delta2.shape,'f2.T=',f2.T.shape,'d2=',d2.shape
-        #print 'delta2=',delta2.shape,'f1.T=',f1.T.shape
-        #d1=np.dot(f1.T, delta2)/hidden_dim2
-        d1=np.dot(delta2.T, f1)
+        d1+=np.dot(delta2.T, f1).T
         dbias_1=delta2
-        #print 'delta1=',delta1.shape,'f1.T=',f1.T.shape,'d1=',d1.shape
-        #print 'delta1=',delta1.shape,'X.T=',X.T.shape
-        d0=np.dot(delta1.T, X)
+        #print 'd0=',d0.shape, 'delta1.T=',delta1.T.shape,'X=', X.shape
+        d0+=np.dot(delta1.T, X).T
         dbias_0=delta1
-        #print 'delta1=',delta1.shape,'X=',X.T.shape,'d0=',d0.shape
-        #print 'synapse_0=',synapse_0.shape,'d0=',d0.shape
-        synapse_0-=alpha*d0.T
-        synapse_1-=alpha*d1.T
-        synapse_2-=alpha*d2.T
-        bias_0-=alpha*dbias_0
-        bias_1=alpha*dbias_1
-        bias_2=alpha*dbias_2
-        #print 'guess = ',pred, 'label = ',y,' error=',error
+        weight0-=alpha*(d0+lamda*weight0)/hidden_dim1
+        weight1-=alpha*(d1+lamda*weight1)/hidden_dim2
+        weight2-=alpha*(d2+lamda*weight2)/output_dim
+        bias_0-=alpha*dbias_0/hidden_dim1
+        bias_1-=alpha*dbias_1/hidden_dim2
+        bias_2-=alpha*dbias_2/output_dim
+    print '----train: ',k,' finish----'
+    rightSum=0
+    wrongSum=0
+    for j in range(valiNum):
+    #for j in range(1):
+        #read a 28x28 image and a byte label
+        #X=np.fromfile(imagef, np.uint8, imgRow*imgCol)
+        #y=np.fromfile(labelf, np.uint8, 1)
+        X=Xa[j]
+        X=X.reshape(1,imgRow*imgCol)
+        X=np.c_[[1],X]
+        y=Ya[j]
+        # where we'll store our best guess (binary encoded)
+        d = np.zeros_like(y)
+        #Forward propagation
+        z1=np.dot(X, weight0)
+        f1=sigmoid(z1+bias_0)
+        f1=np.c_[[1],f1]
+        z2=np.dot(f1, weight1)
+        f2=sigmoid(z2+bias_1)
+        f2=np.c_[[1],f2]
     
-        #if(j%1000 == 0):
-    print 'overallerror=',overallError
+        z3=np.dot(f2, weight2)
+        f3=sigmoid(z3+bias_2)
+        p3=softmax(f3)
+        dd=np.zeros((1, output_dim))
+        indexd=np.argmax(p3)
+        yy=np.zeros((1, output_dim))
+        if indexd==y[0]:
+            rightSum+=1
+        else:
+            wrongSum+=1
     
+    print 'train',k,' right: ',rightSum,'Wrong: ',wrongSum
+    imagef.close()
+    labelf.close()
+print 'overallerror=',overallError
+#plt.plot(range(loop), errordot, "o")
+#plt.show()
+  
 
 print 'train finish'
 
@@ -161,9 +180,9 @@ labelf.close()
 #recordf_0 = open('/home/rdeng/code/mine/nn/myparam0', 'wb')
 #recordf_1 = open('/home/rdeng/code/mine/nn/myparam1', 'wb')
 #recordf_2 = open('/home/rdeng/code/mine/nn/myparam2', 'wb')
-#synapse_0.tofile(recordf_0)
-#synapse_1.tofile(recordf_1)
-#synapse_2.tofile(recordf_2)
+#weight0.tofile(recordf_0)
+#weight1.tofile(recordf_1)
+#weight2.tofile(recordf_2)
 
 testImagef = open('/home/rdeng/code/mine/nn/data/t10k-images-idx3-ubyte', 'rb')
 testLabelf = open('/home/rdeng/code/mine/nn/data/t10k-labels-idx1-ubyte', 'rb')
@@ -173,7 +192,6 @@ timgRow, timgCol =struct.unpack(">II", testImagef.read(8))
 print tmagic, timgNum, timgRow, timgCol
 tlblMagic, tlblNum=struct.unpack(">II", testLabelf.read(8))
 print tlblMagic, tlblNum
-#for j in range(imgNum):
 rightSum=0
 wrongSum=0
 for j in range(timgNum):
@@ -183,29 +201,23 @@ for j in range(timgNum):
     y=np.fromfile(testLabelf, np.uint8, 1)
     # where we'll store our best guess (binary encoded)
     d = np.zeros_like(y)
-    #img = X.reshape(imgRow, imgCol)
-    #show(img)
     X=X.reshape(1, imgRow*imgCol)
-    #print 'X= ',X.shape
-    #print 'y=',y
-    #print 'y.shape= ',y.shape, 'y = ', y
-    #print 'synapse_0= ',synapse_0.shape
+    X=np.c_[[1],X]
+    
     #Forward propagation
-    z1=np.dot(X, synapse_0)
+    z1=np.dot(X, weight0)
     f1=sigmoid(z1+bias_0)
-    #print 'f1= ',f1.shape
-    #print 'synapse_1= ',synapse_1.shape
-    z2=np.dot(f1, synapse_1)
+    f1=np.c_[[1],f1]
+    z2=np.dot(f1, weight1)
     f2=sigmoid(z2+bias_1)
+    f2=np.c_[[1],f2]
 
-    z3=np.dot(f2, synapse_2)
+    z3=np.dot(f2, weight2)
     f3=sigmoid(z3+bias_2)
     p3=softmax(f3)
     dd=np.zeros((1, output_dim))
     indexd=np.argmax(p3)
-    #dd[1,indexd]=1
     yy=np.zeros((1, output_dim))
-    #yy[0,y]=1
     if indexd==y:
         rightSum+=1
     else:
